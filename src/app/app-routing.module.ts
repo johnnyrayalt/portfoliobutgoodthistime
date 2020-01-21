a79d9de9e22ea435dc447525b18f6d8460ea8ba4
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-01-20 19:02:38 -0800

    add new modules to declarations and set routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DefaultComponent } from '@app/layouts/default/default.component';
+import { ArtProjectGridComponent } from './modules/art-project-grid/art-project-grid.component';
+import { WebProjectGridComponent } from './modules/web-project-grid/web-project-grid.component';
 
 import { AboutComponent } from '@app/modules/about/about.component';
 import { ProjectsComponent } from '@app/modules/projects/projects.component';
@@ -13,6 +15,14 @@ const routes: Routes = [
 			{
 				path: 'projects',
 				component: ProjectsComponent,
+			},
+			{
+				path: 'projects/web',
+				component: WebProjectGridComponent,
+			},
+			{
+				path: 'projects/art',
+				component: ArtProjectGridComponent,
 			},
 			{
 				path: 'about',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { WebProjectGridComponent } from './modules/web-project-grid/web-project-grid.component';
 
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
 import { DefaultModule } from '@app/layouts/default/default.module';
+import { ArtProjectGridComponent } from './modules/art-project-grid/art-project-grid.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, ArtProjectGridComponent, WebProjectGridComponent],
 	imports: [BrowserModule, AppRoutingModule, DefaultModule],
 	providers: [],
 	bootstrap: [AppComponent],
