360250982ae5efced8e8b219b24b2364b1db2d7e
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-01-24 12:52:11 -0800

    remove config from git Please enter the commit message for your changes. Lines starting

---
+++ src/services/AWS/AwsService.ts
@@ -0,0 +1,11 @@
+import * as AWS from 'aws-sdk';
+// import * as config from './config.json';
+// let s3 = new AWS.S3();
+
+// var config = new AWS.Config({
+// 	accessKeyId: 'AKID',
+// 	secretAccessKey: 'SECRET',
+// 	region: 'us-west-2',
+// });
+
+// console.log(config);
