e9f4253f1c8767e8daf4667e767f2227ffcdedce
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-01-24 12:12:03 -0800

    update info and styles

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import './AboutStyles.scss';
 
 const About = () => {
-	return <h1>about</h1>;
+	return (
+		<div className="about-text-container">
+			<div className="text">
+				<span className="unicode">&#x261E;</span> b. 1991 Los Angeles, California
+			</div>
+			<div className="text">
+				<span className="unicode">&#x263B;</span> Currently lives and works in Portland, Oregon
+			</div>
+			<div className="text">
+				<span className="unicode">&#x2668;</span> Software Engineer
+			</div>
+		</div>
+	);
 };
 
 export default About;
