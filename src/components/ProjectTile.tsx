ada2324d71f53e6bcb18f08ab45f2c873fdd8869
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-01-27 20:15:41 -0800

    correctly display title and year

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import { IProjectTile } from '../interfaces/IProjectTile';
 import './ProjectTileStyles.scss';
+import { Constants } from '../assets/Constants';
 
 const ProjectTile = (props: IProjectTile): JSX.Element => {
+	let projectDetails: { [name: string]: string };
+
 	const backgroundImage = {
 		backgroundImage: `url(${props.image})`,
 	};
 
-	// const onHover = () =>
+	((name: string): { [name: string]: string } => {
+		const regex: RegExp = /\b([^/]*\/)/gi;
+		const names: RegExpMatchArray | null = name.match(regex);
+
+		if (names === null) {
+			throw new Error(
+				`String does not meet regex requirements: ${name}.
+                Please format as such: root/projectFolder/index.ext`,
+			);
+		}
+
+		const key: string | undefined = names.pop()?.slice(0, -1);
+
+		if (!key) {
+			throw new Error(`Name formatter returned undefined for name: ${name}`);
+		} else {
+			projectDetails = Constants.projects.art[key];
+			console.log(projectDetails);
+			return projectDetails;
+		}
+	})(props.name);
+
 	return (
 		<div className="project-tile-container">
-			<div style={backgroundImage} className="project-tile-image" />
+			<div className="project-tile-image" />
 			<div className="project-tile-text-container">
-				<p className="project-tile-text">{props.name}</p>
+				<p className="project-tile-text">{projectDetails.name}</p>
+				<div className="project-tile-text">{projectDetails.year}</div>
 			</div>
 		</div>
 	);
