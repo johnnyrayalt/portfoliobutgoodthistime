d711167f53c419b73f1a1b40bf464dfeb1a3daa0
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-01-27 20:16:31 -0800

    source info for projects

---
+++ src/assets/Constants.ts
@@ -0,0 +1,29 @@
+export interface ConstantsSchema {
+	projects: {
+		art: {
+			[name: string]: {
+				[name: string]: string;
+			};
+		};
+	};
+}
+
+export const Constants: ConstantsSchema = {
+	projects: {
+		art: {
+			alphaDecay: {
+				key: 'alphaDecay',
+				name: 'Alpha Decay',
+				year: '2013',
+				description:
+					'Installation for my senior thesis at Pacific Northwest College of Art. This installation contains five discrete sculptures, found objects, a video loop, and four poems',
+			},
+			optimizedLivingSpaces: {
+				key: 'optimizedLivingSpaces',
+				name: 'Optimized Living Spaces #1',
+				year: '2012',
+				description: 'Paint and screen print on paper with pencil',
+			},
+		},
+	},
+};
