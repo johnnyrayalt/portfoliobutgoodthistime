b2acb5166d73d2bb7230d50987d266b39d8baf98
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-01-27 22:18:02 -0800

    output images to IndividualProject component

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Constants } from '../assets/Constants';
 import { IProjectTile } from '../interfaces/IProjectTile';
 import './ProjectTileStyles.scss';
-import { Link } from 'react-router-dom';
 
 const ProjectTile = (props: IProjectTile): JSX.Element => {
 	let projectDetails: { [name: string]: string };
