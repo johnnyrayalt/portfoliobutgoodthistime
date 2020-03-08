f94d15594137b83d98cebb7f338ab9600aa999c4
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-03-07 17:59:37 -0800

    add text rendering

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,8 @@
 import React, { RefObject } from 'react';
 import * as THREE from 'three';
 import './Home.scss';
+// const createGeometry = require('three-bmfont-text');
+// const loadFont = require('load-bmfont');
 
 class Home extends React.Component<{}, {}> {
 	private canvas: RefObject<any>;
@@ -13,21 +15,32 @@ class Home extends React.Component<{}, {}> {
 	componentDidMount() {
 		const scene = new THREE.Scene();
 		scene.background = new THREE.Color(0xffffff);
-		const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+		const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 		const canvas = this.canvas.current;
 		const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 
 		renderer.setSize(window.innerWidth - 200, window.innerHeight);
 
-		const geometry = new THREE.BoxGeometry(1, 1, 1);
-		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-		const cube = new THREE.Mesh(geometry, material);
-		scene.add(cube);
-		camera.position.z = 5;
+		const fontJSON = require('../assets/fonts/lato_black.typeface.json');
+		console.log(fontJSON);
+		const font = new THREE.Font(fontJSON);
+		const fontGeometry = new THREE.TextGeometry('WELCOME', {
+			font: font,
+			size: 1,
+			height: 1,
+		});
+
+		const fontMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+
+		const fontMesh = new THREE.Mesh(fontGeometry, fontMaterial);
+
+		scene.add(fontMesh);
+
+		camera.position.z = 8;
 		const animate = function() {
 			requestAnimationFrame(animate);
-			cube.rotation.x += 0.01;
-			cube.rotation.y += 0.01;
+			// cube.rotation.x += 0.01;
+			// cube.rotation.y += 0.01;
 			renderer.render(scene, camera);
 		};
 		animate();
