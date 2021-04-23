1ba27d28a686eb404298b898b3f5454e19c8efd8
Johnny Ray Alt <johnnyrayalt@gmail.com>
2021-04-23 16:41:19 -0700

    feature: router for displaying different views

---
+++ src/views/Views.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './home';
+import ProjectPage from './projectPage';
+import Works from './works';
+
+const Views = (props: { images: any }): JSX.Element => {
+	const { images } = props;
+
+	return (
+		<>
+			<Switch>
+				<Route exact path='/' component={() => <Home expandedDefaultState={false} />} />
+
+				<Route
+					exact
+					path='/works/art'
+					component={() => <Works expandedDefaultState={false} worksRootIndex={'art'} />}
+				/>
+
+				<Route
+					exact
+					path='/works/professional'
+					component={() => <Works expandedDefaultState={false} worksRootIndex={'professional'} />}
+				/>
+
+				<Route
+					exact
+					path='/works/art/:id'
+					component={() => <ProjectPage expandedDefaultState={false} root={'art'} allImages={images} />}
+				/>
+
+				<Route
+					exact
+					path='/works/professional/:id'
+					component={() => <ProjectPage expandedDefaultState={false} root={'professional'} allImages={images} />}
+				/>
+
+				<Route
+					exact
+					path='/about'
+					component={() => <div><p>about</p></div>}
+				/>
+
+			</Switch>
+		</>
+	);
+};
+
+export default Views;
