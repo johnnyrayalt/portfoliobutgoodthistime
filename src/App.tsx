6dbbafcc549140c540030b9671b50fcb4a215f01
Johnny Ray Alt <johnnyrayalt@gmail.com>
2021-04-23 16:32:40 -0700

    feature: project initialization components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.scss';
+import { IMAGE_ROOTS } from './assets/constants';
+import { creds } from './awsConfig';
+import { IImageMap } from './interfaces/IImageMap';
+import { IParsedImageMap } from './interfaces/IParsedImageMap';
+import { AwsService } from './services/AwsService';
+import { ImageUtils } from './utils/ImageUtils';
+import Views from './views/Views';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (): JSX.Element => {
+
+	const [parsedImageMaps, setParsedImageMaps] = useState<IParsedImageMap[]>([]);
+
+	const initAws = (): AwsService => {
+		const aws: AwsService = new AwsService(creds);
+		aws.init();
+		return aws;
+	};
+
+	const getImages = async (awsService: AwsService): Promise<IImageMap[]> => {
+		return await awsService.getImageUrlMaps();
+	};
+
+	const parseImages = (imageMaps: IImageMap[]): IParsedImageMap[] => {
+		return ImageUtils.sortIntoGroup(imageMaps, [IMAGE_ROOTS.art, IMAGE_ROOTS.professional]);
+	};
+
+	useEffect(() => {
+		const getData = async (): Promise<void> => {
+			const aws: AwsService = initAws();
+			const imageMaps = await getImages(aws).then((res: IImageMap[]): IImageMap[] => res);
+			setParsedImageMaps(parseImages(imageMaps));
+		};
+
+		getData();
+	}, []);
+
+	return (
+		<div className='App'>
+			<div id='nav-wrapper'>
+				<Views images={parsedImageMaps} />
+			</div>
+		</div>
+	);
+};
 
 export default App;
