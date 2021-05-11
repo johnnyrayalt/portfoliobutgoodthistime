f2140343268e18fae7d0c9837ff30f2d4d7e6dd8
Johnny Ray Alt <johnnyrayalt@gmail.com>
2021-05-11 01:16:39 -0700

    feature: refactor for safer types

--- src/components/imageScroll/index.tsx
+++ src/components/imageScroll/index.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { IMAGE_SIZE_DEFAULTS } from '../../assets/constants';
+import { IImageList } from '../../interfaces/IImageList';
+import { StringObj } from '../../types/StringObj';
 import './styles.css';
 
-const ImageScroll = (props: { imageList: any }): JSX.Element => {
-	const { imageList } = props;
-	console.log(imageList)
+type Props = {
+	imageList: IImageList[],
+	defaultSize: string
+}
+
+const ImageScroll: FC<Props> = (Props): JSX.Element => {
+	const { imageList, defaultSize } = Props;
+
+	const mapImages = (): JSX.Element[] => {
+		return imageList.map((image: IImageList): JSX.Element => {
+			const sizes: string[] = IMAGE_SIZE_DEFAULTS;
+			const returnObj: StringObj = {};
+			let returnString: string = '';
+
+			for (let size of sizes) {
+				returnObj[size] = image.url.replace(defaultSize, size);
+				returnString += ` ${returnObj[size]} ${size}w,`;
+			}
+
+			return (
+				<li key={image.id}>
+					<img
+						className='image'
+						srcSet={returnString.trimLeft().slice(0, -1)}
+						alt={''} />
+				</li>
+			);
+		});
+	};
 
 	return (
 		<div className='image-scroll-container'>
 			<ul>
-				{imageList !== undefined && imageList.map((image: any) => (
-					<li key={image.id}>
-						<img className='image' src={image.url} alt={''} />
-					</li>
-				))}
+				{mapImages()}
 			</ul>
 		</div>
 	);
