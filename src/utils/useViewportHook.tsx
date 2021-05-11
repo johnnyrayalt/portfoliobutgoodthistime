8d069ec20937f7becbf69a97d3f1acfcc6871a8f
Johnny Ray Alt <johnnyrayalt@gmail.com>
2021-05-11 01:15:28 -0700

    feature: hook for getting the viewport width

---
+++ src/utils/useViewportHook.tsx
@@ -0,0 +1,31 @@
+import React, {
+	createContext,
+	FC,
+	useContext,
+	useEffect,
+	useState,
+} from 'react';
+import { IViewport } from '../interfaces/IViewport';
+
+const ViewportContext = createContext<IViewport>({
+	width: window.innerWidth,
+});
+
+export const ViewportProvider: FC = ({ children }) => {
+	const [width, setWidth] = useState<number>(window.innerWidth);
+
+	const handleResize: (() => void) = () => setWidth(window.innerWidth);
+
+	useEffect(() => {
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	return (
+		<ViewportContext.Provider value={{ width }}>
+			{children}
+		</ViewportContext.Provider>
+	);
+};
+
+export const useViewport: (() => IViewport) = () => useContext<IViewport>(ViewportContext);
