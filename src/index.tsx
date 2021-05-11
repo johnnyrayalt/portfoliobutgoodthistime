127448d76b5c87462a176e43e33f1b61d8ef8513
Johnny Ray Alt <johnnyrayalt@gmail.com>
2021-05-11 01:13:35 -0700

    feature: add ViewPort hook provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { ViewportProvider } from './utils/useViewportHook';
 
 ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  document.getElementById('root')
+	<ViewportProvider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</ViewportProvider>,
+	document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
