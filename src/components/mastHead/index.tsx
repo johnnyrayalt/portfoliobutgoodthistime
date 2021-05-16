b2a9e33ff3d0c85119cf41502e11231ae986222e
Johnny Ray Alt <johnnyrayalt@gmail.com>
2021-05-15 17:41:26 -0700

    feature: add mobile nav component

--- src/components/mastHead/index.tsx
+++ src/components/mastHead/index.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from 'react';
+import { useViewport } from '../../utils/useViewportHook';
 import Header from '../header';
+import MobileNav from '../mobileNav';
 import Navigation from '../navigation';
 import './styles.css';
 
 const MastHead: FC = (): JSX.Element => {
+	const { width } = useViewport();
 
 	return (
 		<div className='masthead'>
 			<Header />
-			<Navigation />
+			{(width > 930) ? <Navigation /> : <MobileNav />}
 		</div>
 	);
 };
