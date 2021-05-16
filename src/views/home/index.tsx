914a5e456c0ff652fe4332bbb437ad972002ecd3
Johnny Ray Alt <johnnyrayalt@gmail.com>
2021-05-16 03:37:50 -0700

    feature: add processing sketch

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
+import Processing from '../../components/processing';
+import './styles.css';
 
 const Home: FC = (): JSX.Element => {
 
 	return (
 		<>
 			<div className='home-container'>
-				<p>HOME</p>
+				<Processing />
 			</div>
 		</>
 	);
